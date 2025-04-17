0378e8d8776ea6bd1da9684db63073a3224a229c
Tomlee <98084303+lurensang@users.noreply.github.com>
2025-04-17 10:55:19 +0800

    make tonkenizer a class

--- bpe/Tokenizer.py
+++ bpe/Tokenizer.py
@@ -1,43 +1,99 @@
 
+class Tokenizer:
+    def __init__(self):
+        self.merge_map: dict = {}
+        self.decode_map: dict = {}
 
-def text2bin(content: str) -> list[int]:
-    return list(content.encode('utf-8'))
+    def train(self, text: str, vocab_size: int) -> None:
+        """
+        Train the tokenizer using BPE algorithm.
+        Params:
+            text (str): string-type data used to run BPE.
+            vocab_size (int): the size of final vocabulary.
 
-def get_frequency(content: list[int]) -> dict:
-    freq = {}
-    for pair in zip(content, content[1:]):
-        freq[pair] = freq.get(pair, 0) + 1
-    return freq
+        Return:
+            None
+        """
+        merge_map = {}
+        decode_map = {i: i.to_bytes() for i in range(256)}
+        
+        if vocab_size > 256:
+            iter_num = vocab_size - 256
+            text_byte = self.text2bin(text)
+            for i in range(iter_num):
+                freq = self.get_frequency(text_byte)
+                pair = self.find_max_pair(freq)
+                idx = i + 256
+                text_byte = self.merge(text_byte, pair, idx)
+                merge_map[pair] = idx
+                decode_map[idx] = decode_map[pair[0]] + decode_map[pair[1]]
 
-def merge(content: list[int], pair: tuple[int, int], idx: int) -> list[int]:
-    i = 0
-    new_content = []
-    while i < len(content):
-        if i < len(content) - 1:
-            if (content[i], content[i+1]) == pair:
-                new_content.append(idx)
-                i += 2
-                continue
-        new_content.append(content[i])
-        i += 1
+        # return merge_map, decode_map
+        self.merge_map = merge_map
+        self.decode_map = decode_map
 
-    return new_content
+    def encode(self, text: str) -> list[int]:
+        """
+        Encode the input string into a token list.
+        Params:
+            text (str): data to be tokenized.
 
-def find_max_pair(freq: dict) -> tuple[int,int]:
-    return max(freq, key=freq.get)
+        Return:
+            ids (list): list of integer-type tokens.
+        """
+        ids = self.text2bin(text)
+        while len(ids) > 1:
+            pair = min(ids, key=lambda p:self.merge_map.get(p, len(self.merge_map)))
+            if pair not in self.merge_map:
+                # nothing to merge
+                break
+            else:
+                ids = self.merge(ids, pair, self.merge_map[pair])
 
-def train(content: str, vocab_size: int) -> tuple[dict, dict]:
-    merge_map = {}
-    decode_map = {i: i.to_bytes() for i in range(256)}
+        return ids
+
+
+    def decode(self, ids: list[int]) -> str:
+        """
+        Decode a token list into a string.
+        Params:
+            ids (list): list of integer-type tokens.
+
+        Return:
+            text (str): string-type data.
+        """
+        return self.bin2text(b''.join([self.decode_map[i] for i in ids]))
+
+    @staticmethod
+    def text2bin(content: str) -> list[int]:
+        return list(content.encode('utf-8'))
     
-    if vocab_size > 256:
-        iter_num = vocab_size - 256
-        content_byte = text2bin(content)
-        for i in range(iter_num):
-            freq = get_frequency(content_byte)
-            pair = find_max_pair(freq)
-            idx = i + 256
-            content_byte = merge(content_byte, pair, idx)
-            merge_map[pair] = idx
-            decode_map[idx] = decode_map[pair[0]] + decode_map[pair[1]]
-    return merge_map, decode_map
+    @staticmethod
+    def bin2text(content: bytes) -> str:
+        return content.decode('utf-8', errors='replace')
+
+    @staticmethod
+    def get_frequency(content: list[int]) -> dict:
+        freq = {}
+        for pair in zip(content, content[1:]):
+            freq[pair] = freq.get(pair, 0) + 1
+        return freq
+
+    @staticmethod
+    def merge(content: list[int], pair: tuple[int, int], idx: int) -> list[int]:
+        i = 0
+        new_content = []
+        while i < len(content):
+            if i < len(content) - 1:
+                if (content[i], content[i+1]) == pair:
+                    new_content.append(idx)
+                    i += 2
+                    continue
+            new_content.append(content[i])
+            i += 1
+
+        return new_content
+
+    @staticmethod
+    def find_max_pair(freq: dict) -> tuple[int,int]:
+        return max(freq, key=freq.get)
