9b5b23971eed11a5cfc5c5bfd58b6f03ea148e88
Tomlee <98084303+lurensang@users.noreply.github.com>
2025-05-19 20:15:34 +0800

    use local models

--- finetune_lyx/model.py
+++ finetune_lyx/model.py
@@ -16,12 +16,14 @@ def smart_tokenizer_and_embedding_resize(
 
     Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
     """
+     # adds special tokens
     num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
 
     if num_new_tokens > 0:
-        input_embeddings = model.get_input_embeddings().weight.data
-        output_embeddings = model.get_output_embeddings().weight.data
+        # set new weights to average of previous weights
+        input_embeddings: torch.Tensor = model.get_input_embeddings().weight.data
+        output_embeddings: torch.Tensor = model.get_output_embeddings().weight.data
 
         input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
         output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
@@ -31,13 +33,16 @@ def smart_tokenizer_and_embedding_resize(
 
 
 def get_model_and_tokenizer(model_name_or_path="gpt2", trust_remote_code=False, max_length=1024, use_lora=False, lora_args=None):
+    # loads from local
     model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_name_or_path,
+        # model_name_or_path,
+        'finetune_lyx/gpt2_model',
         torch_dtype="auto",
         trust_remote_code=trust_remote_code,
     )
     tokenizer = transformers.AutoTokenizer.from_pretrained(
-        model_name_or_path,
+        # model_name_or_path,
+        'finetune_lyx/gpt2_tokenizer_hf/',
         model_max_length=max_length,
         padding_side="right"
     )
